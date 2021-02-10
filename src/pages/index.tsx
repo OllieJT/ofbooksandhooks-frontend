2d11f1f79727d93edb665c1d8d3ee7d4feb638a4
Ollie Taylor <olliejt@hey.com>
2021-02-10 02:09:34 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import { GetServerSideProps } from "next";
-
-export const getServerSideProps: GetServerSideProps<any> = async () => {
-	// Fetch data from external API
-	const res = await fetch(`http://localhost:3000/api/hello-world?name=Ollie`);
-	const data = await res.json();
-
-	// Pass data to the page via props
-	return { props: { data } };
-};
 
 const Home = (): React.ReactElement => {
 	return (
