f4e59e02582f4a0f463c528f1d3e7814ee20d8e0
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2021-05-23 18:29:41 +0100

    Adds scroll to top

--- src/components/layout/layout/component.tsx
+++ src/components/layout/layout/component.tsx
@@ -1,18 +1,25 @@
-import { LayoutEditor } from "../layout-editor";
+import { LayoutScrollUp } from "../layout-scrollup";
 import { LayoutNav } from "../layout-nav";
 import style from "./styles.module.scss";
+import { useScrollPercentage } from "react-scroll-percentage";
 
 interface Props {
 	children: React.ReactNode;
 }
 
 export const Layout = ({ children }: Props): React.ReactElement => {
+	const [ref, percentage] = useScrollPercentage({
+		threshold: 0,
+	});
+
 	return (
-		<div className={style.container}>
+		<div className={style.container} ref={ref}>
 			<LayoutNav />
-			<main className={style.main}>{children}</main>
+			<main id="top" className={style.main}>
+				{children}
+			</main>
 			<div className={style.background} />
-			<LayoutEditor />
+			<LayoutScrollUp percentage={percentage} />
 		</div>
 	);
 };
