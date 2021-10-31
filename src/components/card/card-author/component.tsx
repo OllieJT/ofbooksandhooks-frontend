8a0944dad0c5dd2084c38f6459dda58e21f7812c
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2021-10-31 17:58:04 +0000

    Refactored Author Card

--- src/components/card/card-author/component.tsx
+++ src/components/card/card-author/component.tsx
@@ -1,5 +1,4 @@
 import style from "./styles.module.scss";
-import Link from "next/link";
 import type { FixedImage } from "@lib/utility/handle-sanity-image";
 import Image from "next/image";
 
@@ -7,26 +6,23 @@ export interface CardAuthorProps {
 	title: string;
 	subtitle: string;
 	avatar?: FixedImage;
-	cta?: {
-		label: string;
-		url: string;
-	};
 }
 
-export const CardAuthor = ({ title, subtitle, avatar, cta }: CardAuthorProps) => {
+export const CardAuthor = ({ title, subtitle, avatar }: CardAuthorProps) => {
 	return (
 		<article className={style.container}>
-			<header className={style.header}>
-				{avatar && <Image className={style.avatar} src={avatar.url} alt={avatar.alt} width={88} height={88} />}
-				<h4 className={style.title}>{title}</h4>
-			</header>
-			<p className={style.subtitle}>{subtitle}</p>
-
-			{cta && (
-				<Link href={cta.url} passHref>
-					<a className={style.link}>{cta.label}</a>
-				</Link>
+			{avatar && (
+				<Image
+					className={style.avatar}
+					src={avatar.url}
+					alt={avatar.alt}
+					width={120}
+					height={120}
+					objectFit="fill"
+				/>
 			)}
+			<h4 className={style.title}>{title}</h4>
+			<p className={style.subtitle}>{subtitle}</p>
 		</article>
 	);
 };
